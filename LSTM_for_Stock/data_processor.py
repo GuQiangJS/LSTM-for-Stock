9060d1eff1695491121c381c5f7fccd4da93e584
Gu Qiang <GuQiangJS@users.noreply.github.com>
2019-03-14 00:13:54 +0800

    data_processor 数据读取器完成构造训练集

---
+++ LSTM_for_Stock/data_processor.py
@@ -0,0 +1,181 @@
+import datetime
+
+import numpy as np
+from QUANTAXIS import QA_fetch_index_day_adv as Fetch_index_adv
+from QUANTAXIS import QA_fetch_stock_day_adv as Fetch_stock_adv
+from sklearn.preprocessing import Normalizer
+
+
+class DataLoader(object):
+    """數據讀取器
+
+    Args:
+        stock_code (str): 股票代碼
+        benchmark_code (str): 指數代碼
+        split (float): 訓練集+驗證集/測試集的拆分比率。（0<x<1）。默認為0.1。
+                       Example: 0.1 意味著保留10%的數據作為驗證集
+        columns (list): 獲取數據時保留的列名。**會取第一列為結果集**。
+        fillna (str): 填充Nan值所用的method。參考 `pandas.DataFrame.fillna`_。
+                      如果不需要填充則傳入None。
+        fq (str): 是否採用復權數據。默認為 前復權。如果不需要復權則傳入 `None`。
+        dropna : 是否在填充Nan值之後丟棄剩餘的Nan值。
+        start (str): 數據開始日期。數據格式(`%Y-%m-%d`)。默認值 `1990-01-01`。
+        end (str): 數據結束日期。數據格式(`%Y-%m-%d`)。默認值 `當天`。
+
+    .. _pandas.DataFrame.fillna:
+        https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.fillna.html
+    """
+
+    def __init__(self, stock_code,
+                 benchmark_code,
+                 split=0.1,
+                 columns=['close', 'open', 'high', 'low', 'volume'],
+                 fillna='ffill',
+                 dropna=True,
+                 fq='qfq',
+                 start='1990-01-01',
+                 end=datetime.datetime.today().strftime('%Y-%m-%d')):
+        self._stock_df = self._fetch_stock_adv(stock_code, start, end, fq=fq)
+        self._benchmark_df = self._fetch_index_adv(benchmark_code, start, end)
+        self.stock_code = stock_code
+        self.benchmark_code = benchmark_code
+        # 完整數據
+        self.data = self._stock_df[columns].join(self._benchmark_df[columns],
+                                                 rsuffix='_benchmark')
+        if fillna:
+            self.data.fillna(method=fillna, inplace=True)
+        if dropna:
+            self.data.dropna(inplace=True, subset=[self.data.columns[0]])
+        len_split = int(len(self.data) * (1 - split))
+        # # 丟棄了測試用列的數據源
+        # d = self.data.drop(columns=[val_column_name])
+        # 訓練集+驗證集可用數據（丟棄了測試用列）
+        self._X_train = self.data.values[:len_split]
+        # # 訓練集+驗證集測試數據
+        # self._Y_train = self.data[val_column_name].values[:len_split]
+        # assert len(self._X_train) == len(self._Y_train)
+        # 保留的測試集可用數據（丟棄了測試用列）
+        self._X_val = self.data.values[len_split:]
+        # # 測試集測試數據
+        # self._Y_val = self.data[val_column_name].values[len_split:]
+        # assert len(self._X_val) == len(self._Y_val)
+        self.len_train = len(self._X_train)  # 訓練集+驗證集大小
+        # self.len_val = len(self._X_val)  # 保留的測試集大小
+
+    def get_train_data(self, window_size, test_size, normalize):
+        """按照給定的窗口大小獲取訓練集的X,Y。
+        X的大小為窗口大小，Y為窗口期最後一項。
+        Args:
+            test_size: 測試數據大小
+            normalize: 是否執行正則化
+            window_size: 窗口大小
+
+        Returns:
+            np.array,np.array: x:三維數組，第一維尺寸是 總拆分的數量；
+                                           第二維尺寸是 window_size；
+                                           第三維尺寸是 總共的測試特性數據量；
+                               y:二維數組，第一維尺寸與 x 一致。
+                                           第二維尺寸是 test_size。
+        """
+        data_x = []
+        data_y = []
+        for i in range(self.len_train - window_size - test_size):
+            x, y = self._next_window(i, window_size, test_size, normalize)
+            data_x.append(x)
+            data_y.append(y)
+        return np.array(data_x), np.array(data_y)
+
+    def _next_window(self, i, window_size, test_size, normalize):
+        """按照指定起始位置和長度構建X,Y
+        Args:
+            test_size: 結果列長度
+            normalize: 是否執行規則化
+            i: 起始位置
+            window_size: 窗口長度
+        Return:
+            np.array,np.array: x:二維數組，第一維是 window_size 大小；
+                                           第二維是 總共的測試特性數據量；
+                               y:一維數組，test_size 大小。
+                               **為 i+window_size開始往後的 test_size 的值。**
+        """
+
+        '''
+        y取值時會去 self.data 中取值，因為如果 i+window_size+test_size
+        的值超過了 _X_train 的取值範圍時會報錯。
+        但是當 i+window_size+test_size 的值過大，超過了 data 的尺寸時，
+        依然會報錯。這裡就涉及到了構造函數傳入的 split 值，如果過小或者
+        數據集本身就很小而window_size或者test_size過大就可能造成此問題。
+       '''
+        # 首先從i開始取出window_size+test_size長度的數據
+        window = np.copy(self._X_train[i:i + window_size + test_size])
+        if normalize:
+            # 分別對每一列做標準化
+            for j in range(window.shape[1]):
+                window[:, j] = Normalizer().fit_transform([window[:, j]])[0]
+        # 取數據的前一部分為x
+        x = window[:-test_size]
+        # 取數據的最後 test_size 部分的第一列為y。
+        # 第一列被標記在構造函數的columns參數中。
+        y = window[-test_size:, [0]][:, 0]
+        return x, y
+
+    @property
+    def start(self):
+        """獲取當前數據的開始日期。如果沒有數據則返回None。"""
+        return self.data.index[0] if not self.data.empty else None
+
+    @property
+    def end(self):
+        """獲取當前數據的結束日期。如果沒有數據則返回None。"""
+        return self.data.index[-1] if not self.data.empty else None
+
+    def _fetch_stock_adv(self, code, start, end, fq, drop_code=True):
+        """讀取股票日線。
+        讀取時會使用 `QA_DataStruct_Stock_day` 類型 填充 `self._stock_data`
+
+        Args:
+            code: 代碼
+            start: 開始日期。%Y-%m-%d
+            end: 結束日期。%Y-%m-%d
+            fq: 是否採用復權數據。默認為 前復權。如果不需要復權則傳入 `None`。
+            drop_code: 是否丟棄index中的code列
+
+        Returns:
+            pd.DataFrame: 日線數據表
+        """
+        # QA_DataStruct_Stock_day類型
+        self._stock_data = Fetch_stock_adv(code, start, end)
+        if fq == 'qfq':
+            dataframe = self._stock_data.to_qfq()
+        elif fq == 'hfq':
+            dataframe = self._stock_data.to_hfq()
+        else:
+            dataframe = self._stock_data.data
+
+        if drop_code:
+            dataframe = dataframe.reset_index()
+            return dataframe.drop(columns=['code']).set_index('date')
+        else:
+            return dataframe
+
+    def _fetch_index_adv(self, code, start, end, drop_code=True):
+        """讀取指數日線
+        讀取時會使用 `QA_DataStruct_Stock_day` 類型 填充 `self._benchmark_data`
+
+        Args:
+            code: 代碼
+            start: 開始日期。%Y-%m-%d
+            end: 結束日期。%Y-%m-%d
+            drop_code: 是否丟棄index中的code列
+
+        Returns:
+            pd.DataFrame: 日線數據表
+        """
+        # QA_DataStruct_Stock_day類型
+        self._benchmark_data = Fetch_index_adv(code, start, end)
+        if drop_code:
+            return self._benchmark_data.data.reset_index() \
+                .drop(columns=['code']) \
+                .set_index('date')
+        else:
+            return self._benchmark_data.data
---
+++ tests/test_dataLoader.py
@@ -0,0 +1,68 @@
+import datetime
+import unittest
+import random
+import QUANTAXIS as QA
+import numpy as np
+
+from LSTM_for_Stock.data_processor import DataLoader
+
+
+class DataLoaderTestCase(unittest.TestCase):
+    def test_init(self):
+        dataloader = DataLoader('601398', '399300')
+        end = datetime.datetime.today().strftime('%Y-%m-%d')
+        d = QA.QA_fetch_stock_day_adv('601398',
+                                      start='1990-01-01',
+                                      end=end).to_qfq().data
+        for col in ['close', 'open', 'high', 'low', 'volume']:
+            self.assertTrue(np.array_equal(d[col].values,
+                                           dataloader.data[col].values), col)
+        # self.assertEqual(len(dataloader.data_train), int(len(d) * 0.9))
+        # self.assertEqual(len(dataloader.data_val), int(len(d) * 0.1))
+
+    def test_get_train_data(self):
+        dataloader = DataLoader('601398', '399300')
+        d = dataloader.data
+        for z in [[30, 1], [30, 2], [30, 4], [60, 5]]:
+            X, Y = dataloader.get_train_data(z[0], z[1], False)
+            self.assertEqual(X.shape[0], Y.shape[0])
+            self.assertEqual(X.shape[1], z[0])
+            self.assertEqual(Y.shape[1], z[1])
+            for i in range(len(Y)):
+                for j in range(z[1]):
+                    self.assertEqual(Y[i][j], d.iloc[j + i + z[0]]['close'],
+                                     'z={0},i={1}'.format(z, i))
+
+    def test_plot_train_data2(self):
+        """測試繪圖，做規則化的訓練價格和測試價格"""
+        start = random.randint(0,500)
+        window = 60
+        test = 15
+        dataloader = DataLoader('601398', '399300')
+        x, y = dataloader.get_train_data(window, test, True)
+        print(dataloader.data.iloc[start:start + window + test]['close'])
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        sns.lineplot(x=range(len(x[start])), y=x[start][:, 0])
+        sns.lineplot(x=range(len(x[start]), len(x[start]) + len(y[start])),
+                     y=y[start])
+        plt.show()
+
+    def test_plot_train_data1(self):
+        """測試繪圖，不做規則化的訓練價格和測試價格"""
+        start = random.randint(0,500)
+        window = 30
+        test = 5
+        dataloader = DataLoader('601398', '399300')
+        x, y = dataloader.get_train_data(window, test, False)
+        print(dataloader.data.iloc[start:start + window + test]['close'])
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        sns.lineplot(x=range(len(x[start])), y=x[start][:, 0])
+        sns.lineplot(x=range(len(x[start]), len(x[start]) + len(y[start])),
+                     y=y[start])
+        plt.show()
+
+
+if __name__ == '__main__':
+    unittest.main()
