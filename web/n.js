f7df242d99e1db867699210890a89de36be3ad52
Gu Qiang <GuQiangJS@users.noreply.github.com>
2019-04-13 15:52:35 +0800

    修改模板

--- web/n.js
+++ web/n.js
@@ -1,3 +1,14 @@
+function setClass(ele,value)
+{
+	if(value>1)
+	{
+		ele.classList.add("td_up")
+	}
+	else if(value<1)
+	{
+		ele.classList.add("td_down")
+	}
+}
 // Basic example
 $(document).ready(function () {
 $('#dtBasicExample').DataTable({
@@ -5,11 +16,13 @@ $('#dtBasicExample').DataTable({
     "pageLength": 15,
 	"lengthChange":false
 });
-$('.dataTables_length').addClass('bs-select');
+//$('.dataTables_length').addClass('bs-select');
 });
 $(".price").each(function(){
-    this.innerText=numeral(this.innerText).format('0.0000')
+	s=parseFloat(this.innerText);
+    this.innerText=numeral(s).format('0.0000')
 })
 $(".percent").each(function(){
+	setClass(this,parseFloat(this.innerText));
     this.innerText=numeral(this.innerText/100).format('0.00%')
 })
